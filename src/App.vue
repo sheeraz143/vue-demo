8ddf6e86c47bc79d0f2770e4b4f50a52979f3e03
Sheeraz <mdsheeraz143@gmail.com>
2023-01-24 21:17:19 +0530

    push test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld />
+  <SampleFile />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import SampleFile from "./components/SampleFile.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    SampleFile
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <h1>
+    {{ header || "welcome" }}
+  </h1>
+  <label for="new-todo">Add a todo</label>
+  <input
+    v-model="newTodoText"
+    placeholder="E.g. Feed the cat"
+    @keyup.enter="additem"
+  />
+  <p>{{ charactercount }}/200</p>
+  <button @click="additem" :disabled="newTodoText.length === 0">Add</button>
+  <p v-if="todos.length === 0">welcome!!!!</p>
+  <a v-bind:href="todos">dynamic link</a>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      {{ todo.title }}
+      <button @click="removeitem">Remove</button>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      header: "Shopping List App",
+      newTodoText: "",
+      // charactercount: "",
+      todos: [
+        {
+          id: 1,
+          title: "Do the dishes",
+        },
+        {
+          id: 2,
+          title: "Take out the trash",
+          purchase: true,
+        },
+        {
+          id: 3,
+          title: "Mow the lawn",
+        },
+      ],
+      nextTodoId: 4,
+    };
+  },
+  computed: {
+    charactercount() {
+      return this.newTodoText.length;
+    },
+  },
+  methods: {
+    additem() {
+      this.todos.push({ id: this.todos.length + 1, title: this.newTodoText });
+      this.newTodoText = "";
+    },
+    removeitem() {
+      this.todos.pop();
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul li{
+  list-style-type: none;
+}
+</style>
